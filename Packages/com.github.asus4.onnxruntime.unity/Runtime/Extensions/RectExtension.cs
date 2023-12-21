526ebde3d12fc2b4a9203ac11e9b77f69088ba81
Koki Ibukuro <koki.ibukuro@gmail.com>
2023-12-21 16:19:26 +0100

    Refactor Rect IoU extension

--- Packages/com.github.asus4.onnxruntime.unity/Runtime/Extensions/RectExtension.cs
+++ Packages/com.github.asus4.onnxruntime.unity/Runtime/Extensions/RectExtension.cs
@@ -1,45 +1,78 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Microsoft.ML.OnnxRuntime.Unity
 {
+    /// <summary>
+    /// Extension methods for Rect
+    /// </summary>
     public static class RectExtension
     {
+        /// <summary>
+        /// Returns the area of the rectangle
+        /// </summary>
+        /// <param name="rect">A Rect</param>
+        /// <returns>A area</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Area(this in Rect rect)
+        {
+            return Math.Abs(rect.width * rect.height);
+        }
+
+        /// <summary>
+        /// Create intersection rectangle of two rectangles
+        /// </summary>
+        /// <param name="rect0">A rect</param>
+        /// <param name="rect1">Another rect</param>
+        /// <param name="intersection">An intersection rect</param>
+        /// <returns>Return true if the rectangles intersect</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IntersectionRect(this in Rect rect0, in Rect rect1, out Rect intersection)
+        {
+            float intersect_xMin = Math.Max(rect0.xMin, rect1.xMin);
+            float intersect_yMin = Math.Max(rect0.yMin, rect1.yMin);
+            float intersect_xMax = Math.Min(rect0.xMax, rect1.xMax);
+            float intersect_yMax = Math.Min(rect0.yMax, rect1.yMax);
+
+            if (intersect_xMin > intersect_xMax || intersect_yMin > intersect_yMax)
+            {
+                intersection = Rect.zero;
+                return false;
+            }
+            intersection = new Rect(intersect_xMin, intersect_yMin, intersect_xMax - intersect_xMin, intersect_yMax - intersect_yMin);
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the IoU (intersection over union) of two rectangles
+        /// See also:
+        /// https://en.wikipedia.org/wiki/Jaccard_index
+        /// </summary>
+        /// <param name="rect0">A rect</param>
+        /// <param name="rect1">A rect</param>
+        /// <returns>IoU</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float IntersectionOverUnion(this in Rect rect0, in Rect rect1)
         {
-            float sx0 = rect0.xMin;
-            float sy0 = rect0.yMin;
-            float ex0 = rect0.xMax;
-            float ey0 = rect0.yMax;
-            float sx1 = rect1.xMin;
-            float sy1 = rect1.yMin;
-            float ex1 = rect1.xMax;
-            float ey1 = rect1.yMax;
+            // Don't allow inverted rectangles
+            Assert.IsTrue(rect0.width >= 0 && rect0.height >= 0);
+            Assert.IsTrue(rect1.width >= 0 && rect1.height >= 0);
 
-            float xmin0 = Mathf.Min(sx0, ex0);
-            float ymin0 = Mathf.Min(sy0, ey0);
-            float xmax0 = Mathf.Max(sx0, ex0);
-            float ymax0 = Mathf.Max(sy0, ey0);
-            float xmin1 = Mathf.Min(sx1, ex1);
-            float ymin1 = Mathf.Min(sy1, ey1);
-            float xmax1 = Mathf.Max(sx1, ex1);
-            float ymax1 = Mathf.Max(sy1, ey1);
-
-            float area0 = (ymax0 - ymin0) * (xmax0 - xmin0);
-            float area1 = (ymax1 - ymin1) * (xmax1 - xmin1);
+            float area0 = rect0.Area();
+            float area1 = rect1.Area();
             if (area0 <= 0 || area1 <= 0)
             {
                 return 0.0f;
             }
+            if (!IntersectionRect(rect0, rect1, out Rect intersection))
+            {
+                return 0.0f;
+            }
 
-            float intersect_xmin = Mathf.Max(xmin0, xmin1);
-            float intersect_ymin = Mathf.Max(ymin0, ymin1);
-            float intersect_xmax = Mathf.Min(xmax0, xmax1);
-            float intersect_ymax = Mathf.Min(ymax0, ymax1);
-
-            float intersect_area = Mathf.Max(intersect_ymax - intersect_ymin, 0.0f) *
-                                   Mathf.Max(intersect_xmax - intersect_xmin, 0.0f);
-
-            return intersect_area / (area0 + area1 - intersect_area);
+            float intersectArea = intersection.Area();
+            return intersectArea / (area0 + area1 - intersectArea);
         }
     }
 }
