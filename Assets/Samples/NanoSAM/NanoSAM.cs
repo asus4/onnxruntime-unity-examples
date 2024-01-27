ec493f85dba81cb08f06db227e65f886783364c3
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-01-27 19:33:15 +0100

    Implement single point encoder/decoder

--- Assets/Samples/NanoSAM/NanoSAM.cs
+++ Assets/Samples/NanoSAM/NanoSAM.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Microsoft.ML.OnnxRuntime.Unity;
+using Microsoft.ML.OnnxRuntime.Tensors;
 using UnityEngine;
 
 namespace Microsoft.ML.OnnxRuntime.Examples
@@ -37,23 +36,111 @@ namespace Microsoft.ML.OnnxRuntime.Examples
         public void Run(Texture texture, Vector2 point)
         {
             encoder.Run(texture);
-            Debug.Log($"Output: {encoder.Output[0]}");
+            decoder.Run(encoder.ImageEmbeddings, point);
         }
     }
 
     internal sealed class NanoSAMEncoder : ImageInference<float>
     {
-        public Span<float> Output => outputs[0].GetTensorMutableDataAsSpan<float>();
+        public OrtValue ImageEmbeddings => outputs[0];
 
         public NanoSAMEncoder(byte[] model, ImageInferenceOptions options) : base(model, options)
         {
         }
     }
 
-    internal sealed class NanoSAMDecoder : BasicInference
+    internal sealed class NanoSAMDecoder : IDisposable
     {
-        public NanoSAMDecoder(byte[] model, ExecutionProviderOptions options) : base(model, options)
+        private readonly SessionOptions sessionOptions;
+        private readonly RunOptions runOptions;
+        private readonly InferenceSession session;
+
+        private readonly OrtValue[] inputs;
+
+        public NanoSAMDecoder(byte[] model, ExecutionProviderOptions options)
         {
+            try
+            {
+                sessionOptions = new SessionOptions();
+                options.AppendExecutionProviders(sessionOptions);
+                session = new InferenceSession(model, sessionOptions);
+                runOptions = new RunOptions();
+            }
+            catch (Exception e)
+            {
+                session?.Dispose();
+                sessionOptions?.Dispose();
+                throw e;
+            }
+            session.LogIOInfo();
+
+            /*
+            Version: 1.16.3
+            Input:
+            [image_embeddings] shape: 1,256,64,64, type: System.Single
+            [point_coords] shape: 1,-1,2, type: System.Single
+            [point_labels] shape: 1,-1, type: System.Single
+            [mask_input] shape: 1,1,256,256, type: System.Single
+            [has_mask_input] shape: 1, type: System.Single
+
+            Output:
+            [iou_predictions] shape: -1,4, type: System.Single
+            [low_res_masks] shape: -1,-1,-1,-1, type: System.Single
+            */
+
+            // Allocate inputs/outputs
+            var inputMetadata = session.InputMetadata;
+
+            var allocator = OrtAllocator.DefaultInstance;
+            inputs = new OrtValue[]
+            {
+                // image_embeddings
+                null, // shared from encoder
+                // point_coords
+                OrtValue.CreateAllocatedTensorValue(allocator, TensorElementType.Float, new long[] { 1, 1, 2 }),
+                // point_labels
+                OrtValue.CreateAllocatedTensorValue(allocator, TensorElementType.Float, new long[] { 1, 1 }),
+                // mask_input
+                inputMetadata["mask_input"].CreateTensorOrtValue(),
+                // has_mask_input
+                OrtValue.CreateTensorValueFromMemory(new float[]{ 0 }, new long[] { 1 }),
+            };
+        }
+
+        public void Dispose()
+        {
+            runOptions?.Dispose();
+            session?.Dispose();
+            sessionOptions?.Dispose();
+            foreach (var input in inputs)
+            {
+                input.Dispose();
+            }
+        }
+
+        public void Run(OrtValue imageEmbeddings, Vector2 point)
+        {
+            // image_embeddings
+            inputs[0] = imageEmbeddings;
+
+            // point_coords
+            var pointCoords = inputs[1].GetTensorMutableDataAsSpan<float>();
+            pointCoords[0] = point.x;
+            pointCoords[1] = point.y;
+
+            // point_labels
+            var pointLabels = inputs[2].GetTensorMutableDataAsSpan<float>();
+            pointLabels[0] = 0;
+
+            // mask_input and has_mask_input not used
+
+            // Run
+            using var outputs = session.Run(runOptions, session.InputNames, inputs, session.OutputNames);
+            foreach (var output in outputs)
+            {
+                var info = output.GetTensorTypeAndShape();
+                Debug.Log($"Output type:{info.ElementDataType}, shape:[{string.Join(",", info.Shape)}]");
+            }
         }
     }
 }
