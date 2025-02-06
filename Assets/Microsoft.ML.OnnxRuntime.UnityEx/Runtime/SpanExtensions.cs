7b918d4e93f6b6d9137b83cbb47085d4a925ae75
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-02-06 23:16:33 +0100

    Implement extension that sets span to GraphicsBuffer without copy

--- Assets/Microsoft.ML.OnnxRuntime.UnityEx/Runtime/SpanExtensions.cs
+++ Assets/Microsoft.ML.OnnxRuntime.UnityEx/Runtime/SpanExtensions.cs
@@ -1,11 +1,13 @@
 using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace Microsoft.ML.OnnxRuntime.UnityEx
 {
     /// <summary>
-    /// Experimental:
-    /// Accesses Multi-Dimensional Tensor Value in 1D-Span
+    /// Experimental Span Extensions
     /// </summary>
     public static class SpanExtensions
     {
@@ -17,5 +19,20 @@ namespace Microsoft.ML.OnnxRuntime.UnityEx
             => new(span, shape);
         public static ReadOnlySpan3D<T> AsSpan3D<T>(this ref ReadOnlySpan<T> span, int3 shape) where T : unmanaged
             => new(span, shape);
+
+        /// <summary>
+        /// Set Span to GraphicsBuffer without copying
+        /// </summary>
+        /// <param name="buffer">A GraphicsBuffer</param>
+        /// <param name="span">The span data to be set</param>
+        /// <typeparam name="T">The type of data</typeparam>
+        public static void SetData<T>(this GraphicsBuffer buffer, Span<T> span) where T : unmanaged
+        {
+            var handle = AtomicSafetyHandle.Create();
+            var arr = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray(span, Allocator.None);
+            NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref arr, handle);
+            buffer.SetData(arr);
+            AtomicSafetyHandle.Release(handle);
+        }
     }
 }
