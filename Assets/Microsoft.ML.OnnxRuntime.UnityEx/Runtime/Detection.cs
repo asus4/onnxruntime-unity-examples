a2c9f2ea5443f1e3082aaf3595bca8686f0aa105
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-02-06 14:51:44 +0100

    Optimize struct copy in NMS using unsafe pointer

--- Assets/Microsoft.ML.OnnxRuntime.UnityEx/Runtime/Detection.cs
+++ Assets/Microsoft.ML.OnnxRuntime.UnityEx/Runtime/Detection.cs
@@ -1,17 +1,29 @@
 using System;
 using UnityEngine;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Microsoft.ML.OnnxRuntime.Unity;
 
 namespace Microsoft.ML.OnnxRuntime.UnityEx
 {
+    /// <summary>
+    /// Interface for Detection Task
+    /// </summary>
+    /// <typeparam name="T">Detection struct</typeparam>
     public interface IDetection<T> : IComparable<T>
         where T : unmanaged
     {
         int Label { get; }
         Rect Rect { get; }
 
-        public static void NMS<U>(
+        /// <summary>
+        /// Non-Maximum Suppression (Multi-Class)
+        /// </summary>
+        /// <param name="proposals">A list of proposals which should be sorted in descending order</param>
+        /// <param name="result">A result of NMS</param>
+        /// <param name="iouThreshold">A threshold of IoU (Intersection over Union)</param>
+        /// <typeparam name="U">Detection</typeparam>
+        public unsafe static void NMS<U>(
             NativeSlice<U> proposals,
             NativeList<U> result,
             float iouThreshold)
@@ -19,26 +31,35 @@ namespace Microsoft.ML.OnnxRuntime.UnityEx
         {
             result.Clear();
 
-            foreach (U a in proposals)
+            int proposalsLength = proposals.Length;
+            U* proposalsPtr = (U*)proposals.GetUnsafeReadOnlyPtr();
+
+            for (int i = 0; i < proposalsLength; i++)
             {
+                U* a = proposalsPtr + i;
                 bool keep = true;
-                foreach (U b in result)
+
+                for (int j = 0; j < result.Length; j++)
                 {
+                    U* b = (U*)result.GetUnsafeReadOnlyPtr() + j;
+
                     // Ignore different classes
-                    if (b.Label != a.Label)
+                    if (b->Label != a->Label)
                     {
                         continue;
                     }
-                    float iou = a.Rect.IntersectionOverUnion(b.Rect);
+
+                    float iou = a->Rect.IntersectionOverUnion(b->Rect);
                     if (iou > iouThreshold)
                     {
                         keep = false;
+                        break;
                     }
                 }
 
                 if (keep)
                 {
-                    result.Add(a);
+                    result.Add(*a);
                 }
             }
         }
