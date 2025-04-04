b7e3dd0f7651bc15077da762d5692d1214d7e241
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-04-04 13:12:00 +0200

    Implement Phi4 Non MultiModal version

--- Assets/Samples/Phi4Multimodal/Phi4MultiModal.cs
+++ Assets/Samples/Phi4Multimodal/Phi4MultiModal.cs
@@ -1,25 +1,70 @@
+/// Copyright (c) Microsoft Corporation. All rights reserved.
+/// Licensed under the MIT License.
+/// 
+/// Modified by @asus4
+
 using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Microsoft.ML.OnnxRuntimeGenAI;
 using UnityEngine;
+using Microsoft.ML.OnnxRuntime.Unity;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace Microsoft.ML.OnnxRuntime.Examples
 {
+    /// <summary>
+    /// PHI4 Multi Model Inference
+    /// 
+    /// Ported from C# example in GenAI repo
+    /// https://github.com/microsoft/onnxruntime-genai/blob/cb5baa7f5cefa6a8cb804de634280d400d090729/examples/csharp/HelloPhi4MM/Program.cs
+    /// </summary>
     public sealed class Phi4MultiModal : IDisposable
     {
-        readonly OgaHandle ogaHandle;
-        // readonly Config config;
-        // Model model;
+        [Serializable]
+        public class Options
+        {
+            [field: SerializeField]
+            public string ModelPath { get; private set; } = string.Empty;
+
+            [field: SerializeField]
+            public string ProviderName { get; private set; } = string.Empty;
+        }
+
+        readonly Config config;
+        readonly Model model;
+        readonly Tokenizer tokenizer;
+
 
         bool disposed = false;
 
-        private Phi4MultiModal()
+        public Phi4MultiModal(string modelPath, string provider)
         {
-            ogaHandle = new OgaHandle();
-            // config = new Config("aaaaa");
-            // model = new Model(config);
+            // Set ORT_LIB_PATH environment variable to use GenAI
+            OrtUnityEnv.InitializeOrtLibPath();
+
+            // Disabling provider for testing
+            provider = string.Empty;
+            if (string.IsNullOrWhiteSpace(provider))
+            {
+                model = new Model(modelPath);
+            }
+            else
+            {
+                // TODO: Test on Windows / Linux
+                config = new Config(modelPath);
+                config.ClearProviders();
+                config.AppendProvider(provider);
+                if (provider.Equals("cuda"))
+                {
+                    config.SetProviderOption(provider, "enable_cuda_graph", "0");
+                }
+            }
+            tokenizer = new Tokenizer(model);
         }
 
         ~Phi4MultiModal()
@@ -27,33 +72,9 @@ namespace Microsoft.ML.OnnxRuntime.Examples
             Dispose(false);
         }
 
-        public static async Awaitable<Phi4MultiModal> CreateAsync(CancellationToken cancellationToken)
-        {
-            // Simulate async initialization
-            await Awaitable.BackgroundThreadAsync();
-            cancellationToken.ThrowIfCancellationRequested();
-
-            Phi4MultiModal instance = null;
-            try
-            {
-                instance = new Phi4MultiModal();
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"Failed to create Phi4MultiModal: {ex.Message}");
-                throw ex;
-            }
-
-            await Awaitable.MainThreadAsync();
-            cancellationToken.ThrowIfCancellationRequested();
-
-            return instance;
-        }
-
         public void Dispose()
         {
             Dispose(true);
-            Debug.Log($"Phi4MultiModal disposed");
         }
 
         void Dispose(bool disposing)
@@ -64,12 +85,84 @@ namespace Microsoft.ML.OnnxRuntime.Examples
             }
             if (disposing)
             {
-                // config?.Dispose();
-                ogaHandle?.Dispose();
-                // model?.Dispose();
+                tokenizer?.Dispose();
+                model?.Dispose();
+                config?.Dispose();
             }
             disposed = true;
         }
 
+        public static async Awaitable<Phi4MultiModal> InitAsync(Options options, CancellationToken cancellationToken)
+        {
+            if (Debug.isDebugBuild)
+            {
+                // Verbose GenAI Log
+                Environment.SetEnvironmentVariable("ORTGENAI_LOG_ORT_LIB", "1");
+            }
+
+            // Run in BG thread to avoid blocking the Unity thread
+            await Awaitable.BackgroundThreadAsync();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            Phi4MultiModal instance = null;
+            try
+            {
+                instance = new Phi4MultiModal(options.ModelPath, options.ProviderName);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to create Phi4MultiModal: {ex.Message}");
+                instance?.Dispose();
+                throw ex;
+            }
+
+            await Awaitable.MainThreadAsync();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return instance;
+        }
+
+        public async IAsyncEnumerable<string> GenerateStream(
+            string prompt,
+            [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            await Awaitable.BackgroundThreadAsync();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using var sequences = tokenizer.Encode($"<|user|>{prompt}<|end|><|assistant|>");
+            using var tokenizerStream = tokenizer.CreateStream();
+
+            const int minLength = 50;
+            const int maxLength = 500;
+
+            using var generatorParams = new GeneratorParams(model);
+            generatorParams.SetSearchOption("min_length", minLength);
+            generatorParams.SetSearchOption("max_length", maxLength);
+            using var generator = new Generator(model, generatorParams);
+
+            generator.AppendTokenSequences(sequences);
+
+            // Return results in the Unity main thread
+            await Awaitable.MainThreadAsync();
+
+            var outputQueue = new ConcurrentQueue<string>();
+            var generateTask = Task.Run(() =>
+            {
+                while (!generator.IsDone())
+                {
+                    generator.GenerateNextToken();
+                    outputQueue.Enqueue(tokenizerStream.Decode(generator.GetSequence(0)[^1]));
+                }
+            }, cancellationToken);
+
+            while (!generateTask.IsCompleted)
+            {
+                await Awaitable.NextFrameAsync(cancellationToken);
+                if (outputQueue.TryDequeue(out var response))
+                {
+                    yield return response;
+                }
+            }
+        }
     }
 }
--- Assets/Samples/Phi4Multimodal/Phi4MultiModalSample.cs
+++ Assets/Samples/Phi4Multimodal/Phi4MultiModalSample.cs
@@ -1,17 +1,104 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.ML.OnnxRuntime.Examples;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Text;
+using System.Threading;
 
 public class Phi4MultiModalSample : MonoBehaviour
 {
+    [SerializeField]
+    Phi4MultiModal.Options options;
+
+    [SerializeField]
+    TMP_Text label;
+
+    [SerializeField]
+    Button button;
+
+    readonly StringBuilder sb = new();
     Phi4MultiModal inference;
 
     async Awaitable Start()
     {
-        inference = await Phi4MultiModal.CreateAsync(destroyCancellationToken);
+        if (Application.isMobilePlatform)
+        {
+            Debug.LogError("Phi4MultiModal is not supported on mobile yet.");
+            UpdateLabel("Phi4MultiModal is not supported on mobile yet.", true, true);
+            ToggleButton(false);
+            return;
+        }
+
+        ToggleButton(false);
+        UpdateLabel("Now loading LLM model. wait a bit...");
+
+        try
+        {
+            inference = await Phi4MultiModal.InitAsync(options, destroyCancellationToken);
+        }
+        catch (Exception ex)
+        {
+            UpdateLabel($"Failed to initialize: {ex.Message}");
+            Debug.LogException(ex);
+            return;
+        }
+
+        UpdateLabel("Model initialized!");
+        button.onClick.AddListener(OnAskButtonClick);
+        ToggleButton(true);
     }
 
     void OnDestroy()
     {
+        button.onClick.RemoveListener(OnAskButtonClick);
         inference?.Dispose();
+        Debug.Log($"Phi4MultiModal disposed");
+    }
+
+    async void OnAskButtonClick()
+    {
+        string prompt = "What is the capital of France?";
+        await Generate(prompt, destroyCancellationToken);
+    }
+
+    async Awaitable Generate(string prompt, CancellationToken cancellationToken)
+    {
+        ToggleButton(false);
+        UpdateLabel("Generating...", true, true);
+
+        var stream = inference.GenerateStream(prompt, cancellationToken);
+        await foreach (var text in stream)
+        {
+            UpdateLabel(text, false);
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+        UpdateLabel("");
+        UpdateLabel("Done!");
+        ToggleButton(true);
+    }
+
+    void ToggleButton(bool isOn)
+    {
+        button.gameObject.SetActive(isOn);
+    }
+
+    void UpdateLabel(string text, bool isNewLine = true, bool clear = false)
+    {
+        if (clear)
+        {
+            sb.Clear();
+        }
+        if (isNewLine)
+        {
+            sb.AppendLine(text);
+        }
+        else
+        {
+            sb.Append(text);
+        }
+        // Using String Builder to reduce GC
+        label.SetText(sb);
     }
 }
